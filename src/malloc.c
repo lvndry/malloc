72bac43be2449927dcfe193effe2aa8ae7cbcb32
lvndry <monga.4landry@gmail.com>
2019-10-05 20:39:22 +0200

    split blocks

---
+++ src/malloc.c
@@ -0,0 +1,78 @@
+#define _GNU_SOURCE
+#include <sys/mman.h>
+#include <stddef.h>
+#include <unistd.h>
+
+#include "malloc.h"
+
+#define align (((((x) - 1) >> 2) << 2) + 4)
+#define META_SIZE sizeof(struct mem_block)
+#define PAGE_SIZE sysconf(_SC_PAGESIZE)
+
+static void split_block(struct mem_block *block)
+{
+    struct mem_block *next = block->next;
+    next->size = PAGE_SIZE - block->size;
+    next->is_available = 1;
+    next->next = block->next;
+    block->next = next;
+}
+
+static void find_block(struct mem_block *start, size_t size)
+{
+    struct mem_block *ptr = start;
+
+    while (ptr && !(ptr->next && ptr->size >= size))
+    {
+        *start = *ptr;
+        ptr = ptr->next;
+    }
+
+    return ptr;
+}
+
+__attribute__((visibility("default")))
+void *malloc(size_t size)
+{
+    if (size == 0)
+    {
+        return NULL;
+    }
+
+    static struct mem_block *base = NULL;
+
+    if (base == NULL)
+    {
+        base = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        base->size = size;
+        base->is_available = 0;
+        base->data = base + META_SIZE;
+        base->next = base->data + size;
+        split_block(base);
+    }
+    else
+    {
+        find_block(base, size);
+    }
+
+    return (void*)base->data;
+}
+
+// __attribute__((visibility("default")))
+// void free(void *ptr)
+// {
+// }
+
+// __attribute__((visibility("default")))
+// void *realloc(void *ptr,
+//              size_t size)
+// {
+//     return NULL;
+// }
+
+// __attribute__((visibility("default")))
+// void *calloc(size_t nmemb,
+//              size_t size)
+// {
+//     return NULL;
+// }
---
+++ src/malloc.h
@@ -0,0 +1,18 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <stddef.h>
+
+struct mem_block {
+    size_t size;
+    int is_available;
+    char *data;
+    struct mem_block *next;
+};
+
+void *malloc(size_t);
+void free(void*);
+void *realloc(void*, size_t);
+void *calloc(size_t, size_t);
+
+#endif
